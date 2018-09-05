1f26dff85b5575576f5780c30660ef9f482a3742
Jiwon Kim <21600156@handong.edu>
2018-09-05 13:11:45 +0000

    p0905

---
+++ p0905.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+    std::cout << "question1. Is \'space\' important?\nquestion2. What is std::cout?\nquestion3. What is int main? Is there str main?" << std::endl;
+}
