ab25b51ffae7981571d0c2e354c81e5e77821003
Jiwon Kim <21600156@handong.edu>
2018-09-06 05:14:23 +0000

    p0906

---
+++ p0906.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "ls            | 목록확인 (list)"<<std::endl;
+    std::cout << "cp            | 복사 (copy)"<<std::endl;
+    std::cout << "rm            | 삭제 (remove)"<<std::endl;
+    std::cout << "cd            | 디렉토리 (change directory)"<<std::endl;
+    std::cout << ".             | 현재 디렉토리 위치 (directory position)"<<std::endl;
+    std::cout << "..            | 상위 디렉토리 위치"<<std::endl;
+    std::cout << "/             | directory " <<std::endl;
+    std::cout << "mv            | 이동 (move)"<<std::endl;
+    std::cout << "git clone     | 주소" <<std::endl;
+    std::cout << "pwd           | 절대경로\n------------------------------------------------------" <<std::endl;
+    std::cout << "mv nameA nameB        | 파일 이름(nameA>nameB)을 바꾼다" <<std::endl;
+    std::cout << "cd home/              | 계정 이름을 알 수 있다"<<std::endl;
+    std::cout << "cd workspace/         | ~/workspace 로 프로그래밍환경이 바뀜"<<std::endl;
+    std::cout << "<iostream>            | input output stream" << std::endl;
+    std::cout << "#include 뒤 <> 광역 \"\" 이건 뭐지? 사용자가 지정해두됨..? " <<std::endl;
+    std::cout << "" <<std::endl;
+    
+}
