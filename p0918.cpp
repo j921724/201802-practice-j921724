58a1223257f0458eee634cb9d8e399dd16ac8eaf
Jiwon Kim <21600156@handong.edu>
2018-09-18 06:32:21 +0000

    operator ^

---
+++ p0918.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a = 9;
+    int b = 28;
+    int c = a^b;
+    
+    std::cout << "a = 0000 1001" << std::endl;
+    std::cout << "b = 0001 1100" << std::endl;
+    std::cout << "c = 0001 0101, therefore 21. int c is " << c << std::endl;
+    
+}
