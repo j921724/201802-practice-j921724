c3c305de888184b7803e92be9dc9952b8388cfc9
Jiwon Kim <21600156@handong.edu>
2018-10-29 05:05:34 +0000

    sleep work eat

---
+++ 201810/p1029-3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+struct student(){
+    int _id;
+    std::string _name;
+};
+
+void eat(struct student*){ //값에 직접 접근해 바꾸기 위해 pointer로 들고옴(student*)
+
+}
+
+void work(struct student*){
+    
+}
+
+void sleep(struct student*){
+    
+}
+
+int main(){
+    struct student() stuList[20];
+    
+    work();
+    for(int j=0; i<5; i++){
+        
+        for(int i=0; i<20; i++){
+            work();
+        }
+        
+        for(int i=0; i<1, i++){
+            eat();
+        }
+        
+        for(int i=0; i<4; i++){
+            sleep();
+        }
+    }
+}
