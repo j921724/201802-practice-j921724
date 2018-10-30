b357331ddd2f22101ae8a3a1221f23a8b03fec44
Jiwon Kim <21600156@handong.edu>
2018-10-30 06:39:51 +0000

    class question

---
+++ 201810/p1030.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    print("Happy ending midterm!");
+    
+    __init__():
+    self.age;
+    self.name;
+    
+    __add__(int a)
+    return self.age+a
+    
+    __sub__(int a)
+    return self.age-a
+    // 근데 class는 어떻게 만들까?
+}
