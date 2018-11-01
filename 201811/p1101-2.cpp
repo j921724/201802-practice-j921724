8209ce3f2ad0d6ef05494bedeebc6f5b406b94ed
Jiwon Kim <21600156@handong.edu>
2018-11-01 04:36:51 +0000

    pointer new delete

--- 201811/p1101-2.cpp
+++ 201811/p1101-2.cpp
@@ -2,28 +2,32 @@
 
 int main(){
     int* p;
-    std::cout << "p= "<< p << std::endl;
+    std::cout << "int *p\np= "<< p << std::endl;
     std::cout << "&p= "<< &p << std::endl;
     std::cout << "*p= "<< *p << std::endl;
     
     p = new int;
-    std::cout << "p= "<< p << std::endl;
+    std::cout << "\np=new int\np= "<< p << std::endl;
     std::cout << "&p= "<< &p << std::endl;
     std::cout << "*p= "<< *p << std::endl;
     
     delete p;
-    std::cout << "p= "<< p << std::endl;
+    std::cout << "\ndelete p\np= "<< p << std::endl;
     std::cout << "&p= "<< &p << std::endl;
     std::cout << "*p= "<< *p << std::endl;
     
     int* q;
-    std::cout << "q= "<< q << std::endl;
+    std::cout << "\nint*q\nq= "<< q << std::endl;
     std::cout << "&q= "<< &q << std::endl;
     std::cout << "*q= "<< *q << std::endl;
     
     q = p;
-    std::cout << "q= "<< q << std::endl;
+    std::cout << "\nq=p\nq= "<< q << std::endl;
     std::cout << "&q= "<< &q << std::endl;
     std::cout << "*q= "<< *q << std::endl;
     
+    *q = 10;
+    std::cout << "\n*q=10\np= "<< p << std::endl;
+    std::cout << "&p= "<< &p << std::endl;
+    std::cout << "*p= "<< *p << std::endl;
 }
