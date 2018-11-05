5487204f4cefc486a8ebe271090e7cc26e948c63
Jiwon Kim <21600156@handong.edu>
2018-11-05 04:31:20 +0000

    week11 mon

---
+++ 201811/p1105.cpp
@@ -0,0 +1,29 @@
+//#include <>   global 하다
+//#include ""   local 하다
+#include <iostream>
+#include "practice.h"
+
+int main(){
+    
+    return 0;
+}
+
+
+
+//-----------------------------
+
+#ifndef _PRACTICE_H
+#define _PRACTICE_H
+class Object{
+//default: private
+
+public:    
+
+private:
+
+protected:
+
+};
+#endif
+
+practice_h가 define되지 않으면 define하라는 뜻
