c7ea9d6bbbd2da1d73f0fa021aefe2d35a1d85d2
Jiwon Kim <21600156@handong.edu>
2018-11-19 04:49:13 +0000

    week13 mon

--- 201811/Student.cpp
+++ 201811/Student.cpp
@@ -16,9 +16,22 @@ Student::Student(const char* _value)
     _name[strlen(_value)] = '\0';
 }
 
+Student::Student(const Student& rhs)
+{
+    this->_id = rhs._id;
+    this->_name = new char[strlen(rhs.name)+1];
+    strcpy(this->_name, rhs.name);
+    this->_name[strlen(rhs.name)] = '\0';
+}
+
 int Student::get_id()
 {
     return _id;
+}
+
+Student::~Student()
+{
+    strcpy(_name, "!!!!!!");    
 }
 
 void Student::set_id(int _value)
