f184dba93b63c97d3f8bf327ede281c64a46ff83
Jiwon Kim <21600156@handong.edu>
2018-11-19 04:47:28 +0000

    week13 mon

---
+++ 201811/Student.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _STUDENT_H
+#define _STUDENT_H
+
+class Student
+{
+private:
+    int _id;
+    char* _name;
+    
+public:
+    Student(); // 1. Default Constructor Handling
+    Student(const char* _value); // 2. Initializer 
+    // 3. option 3
+    
+public:
+    int get_id();
+    void set_id(int _value);
+    
+    char* get_name();
+};
+#endif
