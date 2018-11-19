b69c8b2a298934ea11d68febabe7180bbab433f9
Jiwon Kim <21600156@handong.edu>
2018-11-19 04:48:18 +0000

    week13 mon

---
+++ 201811/default_copy_constructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Student.h"
+
+int main()
+{
+    
+    
+    
+    
+    Student s("cbchoi");
+    s.set_id(20);
+    
+    std::cout << "s.id:" << s.get_id() << std::endl;
+    std::cout << "s.name:" << s.get_name() << std::endl;
+    std::cout << "s.name -> address" << &(*s.get_name()) << std::endl;
+   
+    Student s2 = s;
+    std::cout << "s2.id:" << s2.get_id() << std::endl;
+    std::cout << "s2.name:" << s2.get_name() << std::endl;
+    std::cout << "s2.name -> address" << &(*s2.get_name()) << std::endl;
+    return 0;
+}
