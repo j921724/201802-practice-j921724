5372c91c3fa481dfb0e55f1f885a79b8f43966ed
Jiwon Kim <21600156@handong.edu>
2018-11-19 04:50:39 +0000

    week13 mon

--- 201811/default_copy_constructor.cpp
+++ 201811/default_copy_constructor.cpp
@@ -3,20 +3,22 @@
 
 int main()
 {
-    
-    
-    
-    
     Student s("cbchoi");
     s.set_id(20);
     
     std::cout << "s.id:" << s.get_id() << std::endl;
     std::cout << "s.name:" << s.get_name() << std::endl;
-    std::cout << "s.name -> address" << &(*s.get_name()) << std::endl;
+    std::cout << "s.name -> address:" << static_cast<void *>(s.get_name()) << std::endl;
    
-    Student s2 = s;
-    std::cout << "s2.id:" << s2.get_id() << std::endl;
-    std::cout << "s2.name:" << s2.get_name() << std::endl;
-    std::cout << "s2.name -> address" << &(*s2.get_name()) << std::endl;
+   {
+        Student s2 = s;
+        std::cout << "s2.id:" << s2.get_id() << std::endl;
+        std::cout << "s2.name:" << s2.get_name() << std::endl;
+        std::cout << "s2.name -> address:" << static_cast<void *>(s2.get_name()) << std::endl;
+   }
+    std::cout << "s.name:" << s.get_name() << std::endl;
+
     return 0;
-}
+} 
+
+
