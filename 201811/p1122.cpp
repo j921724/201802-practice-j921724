a0bcbfe72043d60fb04cb32c6d4bcedfce48d4be
Jiwon Kim <21600156@handong.edu>
2018-11-22 05:16:54 +0000

    week13 thu

--- 201811/p1122.cpp
+++ 201811/p1122.cpp
@@ -1,1 +1,13 @@
-기말 문제 유형: class(open source)를 이용한 프로그래밍
+1) 기말 문제 유형: class(open source)를 이용한 프로그래밍
+2) lecture 9 슬라이드 붙여놓고 중요한 키워드 <보기>를 참고한 빈칸 채우기 퀴즈 있음
+
+* create -> Default Constructor
+* create & initialize   - initializer student(10, "kim");
+                        - student s1 = s2; -> Copy Constructor -> swallow copy
+* initialize -> s1 = s2; operator assignment 
+
+@@ copy constructor 중요함. Adding Assignment Operator and Copy Constructor 알고 있기
+@@ deep copy 열심히 설명하심
+@@ 그와 동시에 default copy constructor 열심히 설명하심..
+
+22~28 page 안봐도 됨
