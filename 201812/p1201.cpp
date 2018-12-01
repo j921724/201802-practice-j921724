e297b6e4a2c87effef085cca5c5be250061c4085
Jiwon Kim <21600156@handong.edu>
2018-12-01 09:00:06 +0000

    ch8

---
+++ 201812/p1201.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+using namespace std;
+
+class record {
+    public:
+        char *name;
+    private:
+        int course1, course2;
+        double avg;
+    public:
+        record(char *str = "", int s1 = 100, int s2 = 100);
+        ~record();
+        void print(void);
+};
+
+record::~record(){
+    delete []name;
+}
+
+record::record(char *str, int s1, int s2) {
+    name = new char[strlen(str)+1];
+    strcpy(name, str);
+    course1 = s1; 
+    course2 = s2;
+    avg = ((double) (s1 + s2)) / 2;
+    
+}
+
+void record::print(void){ ... }
+
+int main( ){
+    record *myRecord= new record( );
+    record *yourRecord= new record("KIM", 90, 100);
+    myRecord->print( );
+    yourRecord->print( );
+    delete myRecord, yourRecord;
+    return 0;
+    
+}
